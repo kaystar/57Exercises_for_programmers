b084840cbba33312972242df363cb848f85a1703
kaystar <k.a.wood91@gmail.com>
2018-02-23 23:31:20 +0000

    Challenge 18 Done!

--- Exercise 19.py
+++ Exercise 19.py
@@ -1,3 +1,15 @@
 # !python3
 
 # BMI Calculator
+
+weight = int(input("What is your weight?\n"))
+height = int(input("What is your height?\n"))
+
+bmi = (weight / (height * height)) * 703
+
+if bmi >= 18.5 and bmi <= 25:
+    print("Your BMI is: {}, you are of normal weight range" .format(bmi))
+elif bmi < 18.5:
+    print("Your BMI is {}, you are underweight, you should see your doctor".format(bmi))
+else:
+    print("Your BMI is {}, you're overweight, you should see your doctor".format(bmi))
