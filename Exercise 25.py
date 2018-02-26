b7ce53499fc0083e6a936ea7f621516b639fce25
Princ3 Rose <k.a.wood91@gmail.com>
2018-02-26 15:28:56 +0000

    Create Exercise 25.py

---
+++ Exercise 25.py
@@ -0,0 +1,21 @@
+# !python3
+
+# Password Strength Indicator
+
+recommendlength = 8
+
+def password_strength(pwd):
+    if pwd.isnumeric() and len(pwd) < recommendlength:
+        print("Your password {}, is a very weak password." .format(pwd))
+    elif pwd.isalpha() and len(pwd) < recommendlength:
+        print("Your password {}, is a weak password." .format(pwd))
+    elif any(char.isdigit() for char in pwd) and len(pwd) >= recommendlength:
+        print("Your password {}, is a strong password." .format(pwd))
+    elif pwd == ('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$%^&]).*$/') and len(pwd) >= recommendlength:
+        print("Your password {}, is a very strong password." .format(pwd))
+    else:
+        print("I'm not too sure.")
+
+while True:
+    password = input("Please input a password: \n")
+    password_strength(password)
