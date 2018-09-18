1fdae737aab7541e7af0f33e4b0644119ceb4350
Kevin Rose <k.a.wood91@gmail.com>
2018-09-18 11:39:07 -0400

    Miniscule refractoring

--- Exercise 28.py
+++ Exercise 28.py
@@ -2,10 +2,15 @@
 # Adding numbers - Exercise 28
 
 list_of_nums = []
-number_count = 5
 
-for i in range(0, number_count):
-    variable = int(input("Please enter a value: \n"))
-    list_of_nums.append(variable)
+while True:
+    try:
+        number_count = int(input("Enter how man numbers you would like to input: "))
+    except ValueError:
+        print("Sorry, I didn't understand that.")
+        continue
+    for i in range(0, number_count):
+        variable = int(input("Please enter a value: \n"))
+        list_of_nums.append(variable)
 
-print("The total of all number entered is: " + str(sum(list_of_nums)))
+    print("The total of all number entered is: " + str(sum(list_of_nums)))
