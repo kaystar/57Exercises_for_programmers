dc96e3837ca42f19a8e654664ce057076c3f57fc
Kevin Anaro-Wood <k.a.wood91@gmail.com>
2019-08-08 19:34:48 -0400

    Update mariokart.py

--- mariokart.py
+++ mariokart.py
@@ -1,18 +1,18 @@
 import time
 
 points = {
-    "1st" : 15,
-    "2nd" : 12,
-    "3rd" : 10,
-    "4th" : 8,
-    "5th" : 7,
-    "6th" : 6,
-    "7th" : 5,
-    "8th" : 4,
-    "9th" : 3,
-    "10th": 2,
-    "11th": 1,
-    "12th": 0
+    1 : 15,
+    2 : 12,
+    2 : 10,
+    4 : 8,
+    5 : 7,
+    6 : 6,
+    7 : 5,
+    8 : 4,
+    9 : 3,
+    10: 2,
+    11: 1,
+    12: 0
 }
 
 player_1_scores = []
@@ -24,16 +24,34 @@ def main():
     player_1 = input("Enter a name for player 1:\n")
     player_2 = input("Enter a name for player 2:\n")
 
-    for i in range(1,8):
-        entry = int(input("Enter position for {} [0 to stop entry] : ".format(player_1)))
+    for i in range(1,9):
+        entry = int(input("Enter positions for {} [0 to stop entry] : ".format(player_1)))
+        player_1_scores.append(entry)
+    
+    for i in range(1,9):
+        entry_2 = int(input("Enter positions for {} [0 to stop entry] : ".format(player_2)))
+        player_2_scores.append(entry_2)
+    
+    calculate(player_1_scores, player_2_scores, player_1, player_2)
 
+def calculate(first_player_scores, second_player_scores, p1, p2):
+    player_1_total = 0
+    player_2_total = 0
+    
 
-    while True:
-        entry = int(input("Enter a number [0 to stop entry] : "))
-        if entry == 0:
-            calculate()
-            break
-        numbers.append(entry)
+    for i in first_player_scores:
+        if i in points:
+            player_1_total += points[i]
+    print(player_1_total)
+
+    for i in second_player_scores:
+        if i in points:
+            player_2_total += points[i]
+    print(player_2_total)
+    
+    print("The total scores are:\n {}: {}\n{}: {}".format(p1, player_1_total, p2, player_2_total))
+
+# def winner(player_1, player)
 
 if __name__ == "__main__":
     main()
