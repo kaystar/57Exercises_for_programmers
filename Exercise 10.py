e4bd85d4b3cc627c0a57ff50c43cbc51fd3ecd1b
Kevin Anaro-Wood <k.a.wood91@gmail.com>
2020-03-03 09:39:51 -0500

    Update Exercise 10.py
    Refactored the calculation function using zip()

--- Exercise 10.py
+++ Exercise 10.py
@@ -8,10 +8,9 @@ def printing(subtot,vat,tot):
 
 def calculation(list_a, list_b):
     tax = 5.5
-    itemTotal1 = list_a[0] * list_b[0]
-    itemTotal2 = list_a[1] * list_b[1]
-    itemTotal3 = list_a[2] * list_b[2]
-    subtotal = itemTotal1 + itemTotal2 + itemTotal3
+    subtotal = 0
+    for x, y in zip(shopList, quantitylist):
+        subtotal += x * y
     round(subtotal, 2)
     h = tax = round(subtotal * tax /100,2)
     round(tax, 2)
@@ -26,6 +25,3 @@ while len(shopList) < length:
     quantitylist.append(quantity)
 
 calculation(shopList,quantitylist)
-
-
-
